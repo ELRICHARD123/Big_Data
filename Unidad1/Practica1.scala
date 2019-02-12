4e77a82d685f3190d3ca7a83c4c48fab09f40af8
Ricardo Villa <ricardo.villa17@tectijuana.edu.mx>
2019-02-11 19:32:04 -0800

    practica 1

---
+++ Unidad1/Practica1.scala
@@ -0,0 +1,41 @@
+var metro=3.8
+//metro: Double = 3.8
+
+3/metro
+res0: Double = 0.7894736842105263
+10/metro
+//res1: Double = 2.6315789473684212
+
+15/metro
+//res2: Double = 3.947368421052632
+
+
+
+
+//Conversión de Dólares a pesos
+var dollars=18.50
+//dollars: Double = 18.5
+
+dollars*3
+//res3: Double = 55.5
+
+dollars*10
+//res5: Double = 185.0
+
+dollars*15
+//res6: Double = 277.5
+
+//Conversión de millas a kilómetros
+var millas=1.61
+//millas: Double = 1.61
+
+millas*3
+//res7: Double = 4.83
+
+millas*10
+//res8: Double = 16.1
+
+millas*15
+//Double = 24.150000000000002
+
+
