b71360575ef2749a85c9a8b54abf8b3691f6cce6
Ricardo Villa <ricardo.villa17@tectijuana.edu.mx>
2019-04-07 20:13:46 -0700

    Oknew

--- Unidad2/Examen3.1.scala
+++ Unidad2/Examen3.1.scala
@@ -14,40 +14,53 @@ import org.apache.spark.sql.types._
 // 
 val struct =
    StructType(
-     StructField("5.1", DoubleType, true) ::
-     StructField("3.5", DoubleType, true) ::
-     StructField("1.4", DoubleType, true) ::
-     StructField("0.2",DoubleType, true) ::
+     StructField("SL", DoubleType, true) ::
+     StructField("SW", DoubleType, true) ::
+     StructField("PL", DoubleType, true) ::
+     StructField("PW",DoubleType, true) ::
      StructField("Iris-setosa", StringType, true) :: Nil)
 
 
-val df1 = spark.read.option("header","false").schema(struct).load("Iris.csv")
+val df1 = spark.read.option("header", "false").schema(struct)csv("Iris.csv")
+
+//val df1 = spark.read.option("header","false").format("csv").schema(struct).load("Iris.csv")
 import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer}
 
 // Convertir strings a valores numericos
 val labelIndexer = new StringIndexer().setInputCol("Iris-setosa").setOutputCol("labels")
 // Convertir todas las columnas a una sola con vectorassembler escogemos solo las numericas
-val VectAs = (new VectorAssembler().setInputCols(Array("5.1","3.5", "1.4","0.2")).setOutputCol("features"))
+val VectAs = (new VectorAssembler().setInputCols(Array("SL","SW", "PL","PW")).setOutputCol("features"))
 //sepawidth,petallength,petalwidth
 //val splits = df.randomSplit(Array(0.6, 0.4), seed = 1234L)
 //val train = splits(0)
 //val test = splits(1)
 
-val Array(training, test) = df1.randomSplit(Array(0.7, 0.3), seed = 12345)
+val Array(training, test) = df1.randomSplit(Array(0.6, 0.4), seed = 1234L)
 import org.apache.spark.ml.Pipeline
 // capas de neuronas 
 val layers = Array[Int](3, 2, 3, 3)
 
 //val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setFeaturesCol("features").setBlockSize(128).setSeed(1234L).setMaxIter(100)
-val multiPerc= new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("labels").setFeaturesCol("features").setBlockSize(128).setSeed(12345).setMaxIter(100)
-
+val multiPerc= new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("labels").setFeaturesCol("features").setPredictionCol("pred").setBlockSize(128).setSeed(1234L).setMaxIter(100)
+//.setPredictionCol("pred")
 
 val pipe = new Pipeline().setStages(Array(labelIndexer,VectAs,multiPerc))
 
 // entrenamos el modelo
-val model = multiPerc.fit(training)
+val model = pipe.fit(training)
 
-val results = model.transform(test)
+//predic
+val reslt = model.transform(test)
+
+//
+reslt.select("SL","SW", "PL","PW","Iris-setosa").show(10)
+reslt.select("pred", "labels")
+//
+val predic = reslt.select("pred", "labels")
+val evaluator = new MulticlassClassificationEvaluator().setLabelCol("labels").setPredictionCol("pred").setMetricName("accuracy")
+
+println("Test set accuracy = " + evaluator.evaluate(predic))
+
 
 
 
